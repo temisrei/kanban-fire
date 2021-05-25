bc5b622fa8a7af3a3b2dd2770bcefed8395191d0
Rei Lin <temisrei@gmail.com>
2021-05-25 17:04:14 +0800

    Adding Firebase to the project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAKbvwTsMSsRaGDbInfZ3Ur3c5Bh4GY5J0",
+    authDomain: "kanbanfire-1fdba.firebaseapp.com",
+    projectId: "kanbanfire-1fdba",
+    storageBucket: "kanbanfire-1fdba.appspot.com",
+    messagingSenderId: "309930062570",
+    appId: "1:309930062570:web:4d3d612599436e25051d93",
+    measurementId: "G-ER7T57T4TH"
+  }
 };
 
 /*
